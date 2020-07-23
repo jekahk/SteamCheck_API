346822a8b48b71d6d291646ab2ee665b08f7e79a
Julius Kähkönen <julius.e.kahkonen@utu.fi>
2020-07-23 17:34:00 +0300

    cert.pem & key.pem

--- __init__.py
+++ __init__.py
@@ -2,9 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+
 def create_app():
     app = Flask(__name__)
     CORS(app)
+
+    @app.route("/")
+    def hello():
+        return "<h1 style='color:blue>Hello There!</h1>"
+
+    if __name__ == "__main__":
+        app.run(ssl_context=('cert.pem', 'key.pem'))
 
     from .views import main
     app.register_blueprint(main)
